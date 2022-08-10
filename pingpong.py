4e85f115b0953f327da5b90ff95971725be2532a
Cao Nguyen Bao Ky <caonguyenbaoky@Caos-MacBook-Air.local>
2022-08-10 21:01:22 +0700

    added the ball

--- pingpong.py
+++ pingpong.py
@@ -1,4 +1,5 @@
 from pygame import* 
+from random import*
 class GameSprite(sprite.Sprite):
     def __init__(self,x,y,width,height,filename,speed):
         super().__init__()
@@ -12,7 +13,7 @@ class GameSprite(sprite.Sprite):
     def draw(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 class Player():
-    def __init__(self,x,y,width,height,filename,speed):
+    def __init__(self,x,y,width,height,filename,Color,speed):
         super().__init__()
         self.image =transform.scale(image.load(filename), (width, height))
 
@@ -21,15 +22,36 @@ class Player():
         self.rect.y = y
 
         self.speed = speed
+        self.color = Color
     def draw(self):
         draw.rect(window,self.color,self.rect)
-class Player_1():
+class Player_1(Player):
     def update(self):
-        keys = key.get.pressed()
-        if keys[K_UP] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_DOWN] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y <= 400:
+            self.rect.y += self.speed
+class Player_2(Player):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y <= 400:
+            self.rect.y += self.speed
+class Ball():
+    def __init__(self, filename, Color, width, height):
+        super().__init__()
+        self.image =transform.scale(image.load(filename), (width, height))
+        self.velocity = [randint(4,8),randint(-8,8)]
+        
+        self.color = Color
+        self.rect = self.image.get_rect()
+    def update(self):
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+    def draw(self):
+        draw.rect(window,self.color, self.rect)
 scr_width = 600
 scr_height = 400
  
@@ -39,11 +61,12 @@ display.set_icon(icon)
 
 window = display.set_mode((scr_width,scr_height))
 background = transform.scale(image.load("white.png"), (scr_width, scr_height))
-ball = GameSprite(350, 250,10,10,"ball.png",5)
 
-paddle_1 = GameSprite(10, 200, 10, 50, "paddle.png", 3)
-paddle_2 = GameSprite(580, 200, 10, 50, "paddle.png", 3)
+ball = Ball('ball.png', (0,0,0), 10, 10)
+paddle_1 = Player_1(10, 200, 10, 50, "paddle.png", (0,0,0), 3)
+paddle_2 = Player_2(580, 200, 10, 50, "paddle.png", (0,0,0), 3)
 
+clock = time.Clock
 run = True
 finish = False
 while run:
@@ -60,4 +83,8 @@ while run:
         paddle_1.draw()
         paddle_2.draw()
         ball.draw()
+    if sprite.collide_rect(paddle_1, ball):
+        ball.speed_x *= -1
+    if sprite.collide_rect(paddle_2, ball):
+        ball.speed_x *= -1
     display.update()
